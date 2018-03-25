6f9dfd1c03ad1e3f09cf83be63373c3d27734a16
Sahana Balram <sahana2008balram@gmail.com>
2018-03-25 15:38:23 -0700

    Create doping function

--- scatter.js
+++ scatter.js
@@ -8,4 +8,24 @@ $(document).ready(function(){
 
     var y = d3.scale.linear().range([0,height]);
     var x = d3.time.scale.range([0,width]);
+
+    var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(d3.time.seconds,30).tickFormat(format);
+    var yAxis = d3.svg.axis().scale(y).orient("left").ticks(d3.time.seconds,30).tickFormat(format);
+
+    // adding an attribute to the scatter plot chart
+    let svg = d3.select("#scatter-plot").append("svg").attr("height", height + margin.top + margin.bottom).attr("width",width + margin.left + margin.right);
+
+    svg.append("rect")
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("width",width + margin.left + margin.right)
+    .attr(x,0)
+    .attr(y,0)
+    .attr("fill", "white")
+    .attr("fill-opacity",0.8);
+
+    svg = svg.append("g").attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+    function doping(arg){
+        return arg !== "" ? "red" : "orange";
+    }
 });
