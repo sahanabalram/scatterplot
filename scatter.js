2ba1c89db3de447e13ac32ed9f5f04786eaa95e7
Sahana Balram <sahana2008balram@gmail.com>
2018-03-25 15:24:50 -0700

    Create url

---
+++ scatter.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    const url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+
+    let margin = {top: 60, left:60, bottom: 60, right:90};
+    let height = 480, width = 780;
+
+    let format = d3.time.format("%M:%S");
+});
