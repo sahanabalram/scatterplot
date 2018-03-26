7e32681460d7e45bf96c16d51f54d60b7a82d0cc
Sahana Balram <sahana2008balram@gmail.com>
2018-03-26 16:40:18 -0700

    Change color

--- scatter.js
+++ scatter.js
@@ -1,117 +1,265 @@
-$(document).ready(function(){
-    const url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+$(document).ready(function () {
+    var url =
+        "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
 
-    let margin = {top: 60, left:60, bottom: 60, right:90};
-    let height = 480, width = 780;
+    //our JavaScript section starts and the first thing that happens is that we set the size of the area that we’re going to use for the chart and the margins;
+    var margin = {
+        top: 60,
+        left: 60,
+        bottom: 60,
+        right: 90
+    };
+    var height = 480,
+        width = 780;
+    //format the time with minutes and seconds
+    var ft = d3.time.format("%M:%S");
 
-    let format = d3.time.format("%M:%S");
+    //The next section declares the functions linear and scale
+    var y = d3.scale.linear().range([0, height]);
+    var x = d3.time.scale().range([0, width]);
 
-    var y = d3.scale.linear().range([0,height]);
-    var x = d3.time.scale.range([0,width]);
+    //The declarations for our two axes are relatively simple
+    //show data (nums) under the x-axis line
+    var xAxis = d3.svg
+        .axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(d3.time.seconds, 30)
+        .tickFormat(ft);
+    //show data (nums) left of the y-axis line
+    var yAxis = d3.svg
+        .axis()
+        .scale(y)
+        .orient("left");
 
-    var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(d3.time.seconds,30).tickFormat(format);
-    var yAxis = d3.svg.axis().scale(y).orient("left").ticks(d3.time.seconds,30).tickFormat(format);
+    /* 
+     * The next block of code selects the id scatterplot-stats on the web page 
+     * and appends an svg object to it of the size 
+     * that we have set up with our width, height and margin’s.
+     */
+    var svg = d3
+        .select("#scatterplot-stats")
+        .append("svg")
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", width + margin.left + margin.right);
 
-    // adding an attribute to the scatter plot chart
-    let svg = d3.select("#scatter-plot").append("svg").attr("height", height + margin.top + margin.bottom).attr("width",width + margin.left + margin.right);
+    svg
+        .append("rect")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("fill", "white")
+        .attr("fill-opacity", 0.8);
+    // It also adds a g element that provides a reference point for adding our axes.
+    svg = svg
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg.append("rect")
-    .attr("height", height + margin.top + margin.bottom)
-    .attr("width",width + margin.left + margin.right)
-    .attr(x,0)
-    .attr(y,0)
-    .attr("fill", "white")
-    .attr("fill-opacity",0.8);
+    //declair the tooltip
+    var tooltip = d3
+        .select("#scatterplot-stats")
+        .append("div")
+        .attr("class", "tooltip");
 
-    svg = svg.append("g").attr("transform","translate(" + margin.left + "," + margin.top + ")");
-
-    function doping(arg){
-        return arg !== "" ? "red" : "orange";
+    /*
+     * If doping allegiance make the circle red else make it orange
+     */
+    function doping(arg) {
+        return arg !== "" ? "purple" : "green";
     }
 
-    function showTooltip(d,i) {
+    /* 
+     * this function is like mouse over. 
+     * If we place the mouse over a circle the tooltip is going to show up.
+     */
+    function showToolTip(d, i) {
         tooltip.style({
-            "height": "125px",
-            "width": "200px",
-            "opacity": "0.9"
+            height: "125px",
+            width: "300px",
+            opacity: 0.9
         });
+        var circle = d3.event.target;
+        var tippadding = 5,
+            tipsize = {
+                dx: parseInt(tooltip.style("width")),
+                dy: parseInt(tooltip.style("height"))
+            };
 
-        let circle = d3.event.target;
-        let tipadding = 5, tipsize = {
-            dx: parseInt(tooltip.style("width")),
-            dy: parseInt(tooltip.style("height"))
-        };
+        tooltip
+            .style({
+                top: d3.event.pageY - tipsize.dy - 5 + "px",
+                left: d3.event.pageX - tipsize.dx - 5 + "px"
+            })
+            .html(
+                "<span><b>" +
+                d.Name +
+                ": " +
+                d.Nationality +
+                "<br/>" +
+                "Place: " +
+                d.Place +
+                " | Time: " +
+                d.Time +
+                "<br/>" +
+                "Year: " +
+                d.Year +
+                "<br/><br/>" +
+                "Doping: " +
+                d.Doping +
+                "</b></span>"
+            );
+    }
 
-        tooltip.style({
-            "top": (d3.event.pageY - tipsize.dy - 5) + px,
-            "left": (d3.event.pageX = tipsize.dx - 5) + px
-        }).html("<span><b>" + d.Name + ":" + d.Nationality + ":" + "<br/>" + "Place: " + d.Place + "| Time:" + d.Time + "<br/>" + "Year: " + d.Year + "<br/><br/>" + "Doping: " + d.Doping + "</b></span>");
+    /* 
+     * This function is like mouse out. 
+     * If we mouse out then the tooltip is hidding
+     */
+    function hideToolTip(d, i) {
+        tooltip
+            .style({
+                height: 0,
+                width: 0,
+                opacity: 0
+            })
+            .html("");
+    }
 
-        function hideTooltip(d, i) {
-            tooltip.style({
-                "height": "0",
-                "width": "0",
-                "opacity": "0"
-            });
-        }
-
-        function openEntry(d) {
-            if(d.url) {
-                let win = window.open(d.url, "_blank");
-                win.focus();
-            }
+    /* 
+     * This function is like click. 
+     * If we click in the circle we are transfering to another site
+     */
+    function openEntry(d) {
+        if (d.URL) {
+            var win = window.open(d.URL, "_blank");
+            win.focus();
         }
     }
 
-    d3.json(url,(error,data)=> {
-        if(error){
+    /* 
+     * d3.json takes the variable url and two more parameters
+     * if error, then throw it
+     * else map the time-date in the horizontal axis and the rank-position in the verticall axis
+     */
+    d3.json(url, (error, data) => {
+        if (error) {
             throw new Error("d3.json error");
         } else {
-            let fastest = d3.min(data.map((item)=> {
-                return format.parse(item.Time);
-            }));
+            var fastest = d3.min(
+                data.map(item => {
+                    return ft.parse(item.Time);
+                })
+            );
+            var slowest = d3.max(
+                data.map(item => {
+                    return ft.parse(item.Time);
+                })
+            );
 
-            let slowest = d3.max(data.map((item) => {
-                return format.parse(item.Time);
-            }));
+            x.domain([slowest, fastest]);
+            y.domain([
+                1,
+                d3.max(data, d => {
+                    return d.Place;
+                }) + 1
+            ]);
+            //Add a "g" element that provides a reference point for adding our axes.
+            svg
+                .append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis)
+                .append("text") //add text to the axis
+                .attr("transform", "translate(" + width + ",-30)")
+                .attr("dy", "1.8em")
+                .attr("text-anchor", "end")
+                .text("Race time for 13.8km");
 
-            x.domain([slowest,fastest]);
-            y.domain([1,d3.max(data,(d) => {
-                return d.Place;
-            }) + 1]);
+            svg
+                .append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text") //add text to the axis
+                .attr("transform", "rotate(-90)")
+                .attr("dy", "-0.8em")
+                .attr("text-anchor", "end")
+                .text("Rank");
 
-            svg.append("g").attr("class","x axis").attr("transform","translate(0," + height + ")").call(xAxis).append("text").attr("transform","translate(0," + width + ", -30)").attr("dy","1.8em").attr("text-anchor","end").text("Race time for 13.8 km");
+            /* 
+             * we add the cyclists to our scatterplot
+             * This block of code creates the cyclists (selectAll(".cyclist")) 
+             * and associates each of them with a data set (.data(data)).
+             * We then append a circle 
+             * with values for x/y position and height/width as configured in our earlier code.
+             * we parse the time and the place
+             */
 
-            svg.append("g").attr("class","y axis").attr("transform","translate(0," + height + ")").call(YAxis).append("text").attr("transform","rotate(-90)").attr("dy","0.8em").attr("text-anchor","end").text("Rank");
+            var cyclist = svg
+                .selectAll(".cyclist")
+                .data(data)
+                .enter()
+                .append("g")
+                .attr("class", "cyclist")
+                .attr("x", d => {
+                    return x(ft.parse(d.Time));
+                })
+                .attr("y", d => {
+                    return y(d.Place);
+                });
 
-            let cyclist = svg.selectAll(".cyclicst").data(data).enter().append("g").attr("class","cyclist").attr("x",(d)=>{
-                return x(format.parse(d.Time));
-            })
-            .attr("y",(d)=>{
-                return y(d.Place);
-            });
+            cyclist
+                .append("circle")
+                .attr("cx", d => {
+                    return x(ft.parse(d.Time));
+                })
+                .attr("cy", d => {
+                    return y(d.Place);
+                })
+                .attr("r", 5)
+                .attr("fill", d => {
+                    return doping(d.Doping);
+                })
+                //call the functions
+                .on("mouseover", showToolTip)
+                .on("mouseout", hideToolTip)
+                .on("click", openEntry);
 
-            cyclist.append("circle").attr("cx", (d)=> {
-                return x(format.parse(d.Time));
-            })
-            .attr("cy", (d)=> {
-                return y(d.Place);
-            })
-            .attr("r",5).attr("fill",(d)=> {
-                return doping(d.Doping);
-            })
-            .on("mouseover",showTooltip).on("mouseout",hideTooltip).on("click",openEntry);
+            //append the text and fix the distance btw the circles and the names
+            cyclist
+                .append("text")
+                .attr("x", d => {
+                    return x(ft.parse(d.Time)) + 7;
+                })
+                .attr("y", d => {
+                    return y(d.Place) + 5;
+                })
+                .text(d => {
+                    return d.Name;
+                });
 
-            cyclist.append("text").attr("x",(d) => {
-                return x(fomrat.parse(d.Time))+ 7;
-            })
-
-            .attr("y",(d) => {
-                return y(d.Place)+ 5;
-            })
-            .text((d) => {
-                return d.Name;
-            });
-        }
+            //right-bottom explainatory text
+            var isDoped = svg
+                .append("g")
+                .attr(
+                    "transform",
+                    "translate(" + (width - 150) + "," + (height - 100) + ")"
+                )
+                .append("text")
+                .attr("x", 10)
+                .attr("y", 5)
+                .attr("fill", "purple")
+                .text("* Doping allegiance");
+            var isNotDoped = svg
+                .append("g")
+                .attr(
+                    "transform",
+                    "translate(" + (width - 150) + "," + (height - 80) + ")"
+                )
+                .append("text")
+                .attr("x", 10)
+                .attr("y", 5)
+                .attr("fill", "green")
+                .text("* No doping allegiance");
+        } //end of else
     });
 });
